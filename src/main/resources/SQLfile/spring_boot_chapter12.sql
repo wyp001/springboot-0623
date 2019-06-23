8d18f0c60531c0f3169d04f15b079cb06c17db9a
wyupeng <wyupeng@linewell.com>
2019-06-23 17:17:57 +0800

    sql脚本

---
+++ src/main/resources/SQLfile/spring_boot_chapter12.sql
@@ -0,0 +1,69 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : work01
+Source Server Version : 50716
+Source Host           : localhost:3306
+Source Database       : spring_boot_chapter12
+
+Target Server Type    : MYSQL
+Target Server Version : 50716
+File Encoding         : 65001
+
+Date: 2019-06-23 17:16:15
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for t_role
+-- ----------------------------
+DROP TABLE IF EXISTS `t_role`;
+CREATE TABLE `t_role` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(255) DEFAULT NULL,
+  `note` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_role
+-- ----------------------------
+INSERT INTO `t_role` VALUES ('1', 'a', '啊');
+
+-- ----------------------------
+-- Table structure for t_user
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user`;
+CREATE TABLE `t_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(255) NOT NULL,
+  `pwd` varchar(255) NOT NULL,
+  `available` int(1) DEFAULT '1' COMMENT '1表示可用，0表示不可用',
+  `note` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_user
+-- ----------------------------
+INSERT INTO `t_user` VALUES ('1', 'aaa', 'e94cf903f6b5d1cbee4dc688537e7ff0fbe3fac161d2d49b61517c16a0cdcd19eada0ac0388ab95f', '1', '密码111');
+INSERT INTO `t_user` VALUES ('2', 'bbb', 'c167e04f33253be9d9132afe154a6bbe0e2ac12e3aedf1330836be15d9a81746f48b082b520405a1', '1', '222');
+INSERT INTO `t_user` VALUES ('3', 'ccc', '', '1', '333');
+INSERT INTO `t_user` VALUES ('4', 'ddd', '', '1', '444');
+
+-- ----------------------------
+-- Table structure for t_user_role
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user_role`;
+CREATE TABLE `t_user_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_user_role
+-- ----------------------------
+INSERT INTO `t_user_role` VALUES ('1', '1', '1');
